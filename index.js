6e3a82fe46645f65be92a2726e41b0cd8b1d2041
王刚 <wanggang@rainbowcn.com>
2020-11-12 20:09:39 +0800

    更新描述

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'plugin-advpng',
   title: 'AdvPNG',
-  description: '用于压缩png格式的图片，压缩速度快、压缩率高，但是出图质量欠佳',
+  description: '用于压缩png格式的图片，出图质量和压缩率都不错，非常适合压缩小图标，压缩大图会耗时比较久。',
   type: 'compressor',
   accepts: ['image/png'],
   extensions: ['png'],
