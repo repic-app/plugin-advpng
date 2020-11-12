5ad1a615dd50847e1908560d17d0ac815c87a627
王刚 <wanggang@rainbowcn.com>
2020-11-12 20:07:39 +0800

    功能基本实现

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'plugin-advpng',
+  title: 'AdvPNG',
+  description: '用于压缩png格式的图片，压缩速度快、压缩率高，但是出图质量欠佳',
+  type: 'compressor',
+  accepts: ['image/png'],
+  extensions: ['png'],
+  process: 'main',
+  main: './lib'
+}
